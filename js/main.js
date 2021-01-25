1962c738a4471ef186e9c745140ff2349c16929b
bektur181 <64388306+bektur181@users.noreply.github.com>
2021-01-25 16:41:33 +0600

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,6 @@
+$(function() {
+    $('.top-slider').slick({
+       prevArrow: '<button type="button" class="slick-prev"><img src="images/arrow-prev.svg" alt=""></button>',
+       nextArrow: '<button type="button" class="slick-next"><img src="images/arrow-next.svg" alt=""></button>'
+    });
+});
